ac3e35ef15393b509a678122dc5c08ea78c7c137
Ahmad <ibrahimahmadism@gmail.com>
2025-07-16 20:58:59 +0000

    Remove DynamoDB table resource for Terraform state locking

--- infra-as-code/terraform/sample-aws/remote-state/main.tf
+++ infra-as-code/terraform/sample-aws/remote-state/main.tf
@@ -16,16 +16,3 @@ resource "aws_s3_bucket_versioning" "versioning" {
     status = "Enabled"
   }
 }
-
-
-resource "aws_dynamodb_table" "terraform_state_lock" {
-  name           = "${var.bucket_name}"
-  read_capacity  = 1
-  write_capacity = 1
-  hash_key       = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
