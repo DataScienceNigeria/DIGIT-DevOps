41035d072622d7dbc8e7ee87811a2c8ccd2a4c51
Muhammad Abiodun SULAIMAN <abiodun.msulaiman@gmail.com>
2025-07-22 17:07:21 +0000

    Update configuration files with production values for database, domain, and S3 bucket

--- infra-as-code/terraform/sample-aws/variables.tf
+++ infra-as-code/terraform/sample-aws/variables.tf
@@ -5,7 +5,7 @@
 
 variable "cluster_name" {
   description = "Name of the Kubernetes cluster"
-  default = <cluster_name> #REPLACE
+  default = "digit-lts-dsn" #REPLACE
 }
 
 variable "vpc_cidr_block" {
@@ -63,12 +63,12 @@ variable "max_worker_nodes" {
 
 variable "db_name" {
   description = "RDS DB name. Make sure there are no hyphens or other special characters in the DB name. Else, DB creation will fail"
-  default = <db_name> #REPLACE
+  default = "digitltsdsbdb" #REPLACE
 }
 
 variable "db_username" {
   description = "RDS database user name"
-  default = <db_username> #REPLACE
+  default = "digitltsdsn" #REPLACE
 }
 
 variable "ami_id" {
